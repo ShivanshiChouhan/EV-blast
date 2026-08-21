668e8362ec407efbaab4114985826b5f43381b64
Shivanshi Chouhan <shivanshichouhan11@gmail.com>
2026-08-21 18:43:46 +0530

    Update ev_blast_deployment.py

--- ev_blast_deployment.py
+++ ev_blast_deployment.py
@@ -27,9 +27,15 @@ Poor_Cell_Design=st.number_input(
     min_value=0,
     max_value=100,
     value=0,
-    step=1)
+    step=1
+)
 External_Abuse=st.selectbox("External Abuse",["Yes","No"])
-Poor_Battery_Design=st.number_input("Poor Battery Design",min_value=0.0,max_value=1.0,step=1)
+Poor_Battery_Design=st.number_input(
+    "Poor Battery Design",
+    min_value=0,
+    max_value=1,
+    value=0,
+    step=1)
 Short_Circuits=st.number_input("Short Circuits",min_value=0.0,max_value=1.0,step=1)
 Temperature=st.number_input("Temperature")
 Overcharge_Overdischarge=st.selectbox("Overcharge Overdischarge")
